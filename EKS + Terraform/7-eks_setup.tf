18194207adb553c5924616386013ce208535fc98
ducnguyen1707 <ducngitech@gmail.com>
2025-12-26 16:06:04 +0700

    Add IAM role, policy, public +private api endpoint for workernode (IAM auth)

---
+++ EKS + Terraform/7-eks_setup.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_role" "eks" {
+  name = "${local.env}-${local.eks_name}-eks-cluster"
+  assume_role_policy =  <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_eks_cluster" "eks" {
+  name = "${local.env}-${local.eks_name}-eks-cluster"
+  version = local.eks_version
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access = true
+
+    subnet_ids = [    # deploy worker on private subnet
+        aws_subnet.private_zone1.id,
+        aws_subnet.private_zone2.id
+    ]
+  }
+
+  access_config {
+    # authentication_mode = "API": Uses AWS Identity and Access Management (IAM) for cluster access
+    authentication_mode = "API"
+    
+    # Allows the cluster creator to manage RBAC roles and add other users without manual kubectl configuration
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks
+  ]
+}
